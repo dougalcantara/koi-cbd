aa96ff0efb2ad60f4dbad5e8adab5c62903df4ad
dougalcantara <dougalcantara619@gmail.com>
2019-11-15 14:59:47 -0800

    process-preview visual bug

--- partials/cta-takeover.php
+++ partials/cta-takeover.php
@@ -10,8 +10,8 @@
       <div class="k-ctabanner--content k-block k-block--md">
         <div class="k-preheading k-upcase">More About CBD</div>
         <h3 class="k-headline k-headline--sm">New to CBD? Our Beginner's Guide to CBD Oil Is A Great Place to Start.</h3>
-        <a href="#0" class="k-button k-button--dark">Find Out More &nbsp; &rarr;</a>
-        <a href="#0" class="k-button k-button--secondary">Explore CBD 101 &nbsp; &rarr;</a>
+        <!-- <a href="<?php ?>" class="k-button k-button--dark">Find Out More &nbsp; &rarr;</a> -->
+        <a href="<?php echo site_url() . '/cbd-101'; ?>" class="k-button k-button--primary">Explore CBD 101 &nbsp; &rarr;</a>
       </div>
     </div>
   </div>
