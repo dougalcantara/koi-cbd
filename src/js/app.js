9844f4f45c6b4bd6ac67e34a665a3c26ff795d82
Sebastian Opperman <oppermansebastian@gmail.com>
2019-11-24 13:35:28 -0800

    qty updates

--- src/js/app.js
+++ src/js/app.js
@@ -27,6 +27,7 @@ import './components/video-players';
 import './components/newsletter-signup';
 import './components/newsletter-cta';
 import './components/location-finder';
+import './components/checkout-validation';
+import './components/prod-qty';
 
 import 'promise-polyfill/src/polyfill';
-import './dirty';
