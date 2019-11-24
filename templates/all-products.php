9e18ac899b24d4e69c3b6df58ad47f3ada62d894
Sebastian Opperman <oppermansebastian@gmail.com>
2019-11-23 22:36:04 -0800

    uncategorized removed

--- templates/all-products.php
+++ templates/all-products.php
@@ -44,28 +44,31 @@ $all_categories = get_categories($args);
   <?php
   foreach($all_categories as $i => $product_cat) :
     $cat_fields = get_fields('term_' . $product_cat->cat_ID); ?>
+    <?php if($product_cat->name != 'Uncategorized'): ?>
     <div class="k-productcard">
-    <div class="k-productcard--liner">
+      <div class="k-productcard--liner">
 
-      <a href="<?php echo site_url() . '/' . $links[$i]; ?>">
-        <figure class="k-figure">
-          <div class="k-figure--liner">
-            <img class="k-figure--img" data-src="<?php echo $cat_fields['category_featured_image']['url']; ?>" alt="">
-          </div>
-        </figure>
-      </a>
+        <a href="<?php echo site_url() . '/' . $links[$i]; ?>">
+          <figure class="k-figure">
+            <div class="k-figure--liner">
+              <img class="k-figure--img" data-src="<?php echo $cat_fields['category_featured_image']['url']; ?>" alt="">
+            </div>
+          </figure>
+        </a>
 
-      <div class="k-productcard--title">
-        <h3 class="k-headline k-headline--fake k-weight--lg"><a href="<?php echo site_url() . '/' . $links[$i]; ?>"><?php echo $product_cat->name; ?></a></h3>
-        <p class="k-accent-text k-rte-content"><?php echo $product_cat->description; ?></p>
+        <div class="k-productcard--title">
+          <h3 class="k-headline k-headline--fake k-weight--lg"><a href="<?php echo site_url() . '/' . $links[$i]; ?>"><?php echo $product_cat->name; ?></a></h3>
+          <p class="k-accent-text k-rte-content"><?php echo $product_cat->description; ?></p>
+        </div>
+
+        <div class="k-productcard--action">
+          <a href="<?php echo site_url() . '/' . $links[$i]; ?>" class="k-button k-button--default">Shop Now</a>
+        </div>
+
       </div>
-
-      <div class="k-productcard--action">
-        <a href="<?php echo site_url() . '/' . $links[$i]; ?>" class="k-button k-button--default">Shop Now</a>
-      </div>
-
     </div>
-  </div>
+
+  <?php endif; ?>
   <?php endforeach; ?>
   </div>
 </section>
