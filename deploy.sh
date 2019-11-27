5f66d40925e6b3b9a5fc402223fb4fb25e593160
Sebastian Opperman <oppermansebastian@gmail.com>
2019-11-26 18:23:28 -0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 ssh -t koicbddev@koicbddev.ssh.wpengine.net  'cd /home/wpe-user/sites/koicbddev/wp-content/themes/;rm -rf KOI-2019/*;exit;bash'
-rsync -r --progress  --exclude=".*" --exclude="./node_modules" ./ koicbddev@koicbddev.ssh.wpengine.net:/home/wpe-user/sites/koicbddev/wp-content/themes/KOI-2019
+rsync -r --progress  --exclude=".*" --exclude="node_modules" ./ koicbddev@koicbddev.ssh.wpengine.net:/home/wpe-user/sites/koicbddev/wp-content/themes/KOI-2019
