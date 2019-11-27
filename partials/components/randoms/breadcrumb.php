cddf26ae628654e494d545126331b382c932b76b
dougalcantara <dougalcantara619@gmail.com>
2019-11-26 19:10:31 -0800

    add caret && wc_breadcrumb

--- partials/components/randoms/breadcrumb.php
+++ partials/components/randoms/breadcrumb.php
@@ -1,5 +1,5 @@
 <div class="k-breadcrumb k-block k-block--sm">
   <div class="k-inner k-inner--md">
-    <?php yoast_breadcrumb( '<p id="breadcrumbs">','</p>' ); ?>
+    <?php woocommerce_breadcrumb(array('delimiter' => ' &rsaquo; ')); ?>
   </div>
 </div>
