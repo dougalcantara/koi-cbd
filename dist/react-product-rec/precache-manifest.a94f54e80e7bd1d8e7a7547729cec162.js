f6ae01d3a9a748372aeebc012e8f86036d65279c
dougalcantara <dougalcantara619@gmail.com>
2019-12-03 17:29:47 -0800

    add product rec tool embed

---
+++ dist/react-product-rec/precache-manifest.a94f54e80e7bd1d8e7a7547729cec162.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebebb692e5886d5761bcaded317e0f1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5b103024b8dca96427d",
+    "url": "/static/css/main.4b8bd65b.chunk.css"
+  },
+  {
+    "revision": "dc3be31e104b89690929",
+    "url": "/static/js/2.795fb733.chunk.js"
+  },
+  {
+    "revision": "d5b103024b8dca96427d",
+    "url": "/static/js/main.5502688b.chunk.js"
+  },
+  {
+    "revision": "dd518a629b27381a5e21",
+    "url": "/static/js/runtime-main.7bc131ef.js"
+  }
+]);
