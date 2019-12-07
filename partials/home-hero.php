c31b477730b6e2018fa41ecba64fe08dfa4f513e
Sebastian Opperman <oppermansebastian@gmail.com>
2019-12-07 15:42:01 -0800

    hero assets

--- partials/home-hero.php
+++ partials/home-hero.php
@@ -6,12 +6,12 @@
     <div class="hero-media-img">
       <img
           sizes="(min-width: 30em) 28em, 100vw"
-          srcset="https://res.cloudinary.com/ddxqlbjm1/image/upload/f_auto,q_70,w_256/v1575681780/KoiCBD/koi-cbd-oil-tincture.jpg,
-                https://res.cloudinary.com/ddxqlbjm1/image/upload/f_auto,q_70,w_512/v1575681780/KoiCBD/koi-cbd-oil-tincture.jpg 512w,
-                https://res.cloudinary.com/ddxqlbjm1/image/upload/f_auto,q_70,w_768/v1575681780/KoiCBD/koi-cbd-oil-tincture.jpg 768w,
-                https://res.cloudinary.com/ddxqlbjm1/image/upload/f_auto,q_70,w_1024/v1575681780/KoiCBD/koi-cbd-oil-tincture.jpg 1024w,
-                https://res.cloudinary.com/ddxqlbjm1/image/upload/f_auto,q_70,w_1920/v1575681780/KoiCBD/koi-cbd-oil-tincture.jpg 1920w"
-          src="https://res.cloudinary.com/ddxqlbjm1/image/upload/f_auto,q_70,w_512/v1575681780/KoiCBD/koi-cbd-oil-tincture.jpg"
+          srcset="<?php echo get_template_directory_uri(); ?>/dist/hero-img-256.jpg,
+                <?php echo get_template_directory_uri(); ?>/dist/hero-img-512.jpg 512w,
+                <?php echo get_template_directory_uri(); ?>/dist/hero-img-768.jpg 768w,
+                <?php echo get_template_directory_uri(); ?>/dist/hero-img-1024.jpg 1024w,
+                <?php echo get_template_directory_uri(); ?>/dist/hero-img-1920.jpg 1920w"
+          src="<?php echo get_template_directory_uri(); ?>/dist/hero-img-512.jpg"
           alt />
     </div>
     <div class="hero-media-video">
