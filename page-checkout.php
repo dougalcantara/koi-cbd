deb54a79cd0f039cc8965230c423a5c0e6b9f824
dougalcantara <dougalcantara619@gmail.com>
2020-01-08 17:31:17 -0800

    debug checkout request

--- page-checkout.php
+++ page-checkout.php
@@ -1,6 +1,8 @@
 <?php
 /* Template Name: 2019 Checkout Page */
 
+global $wp;
+
 if (!defined('ABSPATH')) {
 	exit;
 }
@@ -42,6 +44,7 @@ do_action('k_before_first_section');
 
 		<div class="k-checkout__headline">
 			<h1 class="k-headline k-headline--md">Checkout</h1>
+			<?php var_dump($wp->request); ?>
 			<?php if (!is_user_logged_in()) : ?>
 			<div class="woocommerce-form-login-toggle">
 				Returning customer? <a href="<?php echo site_url() . '/login'; ?>">Click here to log in.</a>
