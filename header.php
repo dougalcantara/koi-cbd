ff5f847ebbe241fa01bdae82323e6d1537b1fc6a
dougalcantara <dougalcantara619@gmail.com>
2020-01-08 22:44:16 -0800

    rename deploy to dev script & rm meta noindex tag

--- header.php
+++ header.php
@@ -27,7 +27,6 @@ $current_url = home_url(add_query_arg(array(), $wp->request));
   <!-- Remember to add the live chat script here!! -->
 <!--  <script type="text/javascript" async src="https://cdn.livechatinc.com/tracking.js"></script>-->
 
-  <meta name="robots" content="noindex, nofollow" />
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
