53696f61f836b5263e4c9f2376cec4a7ca493741
Nick Alcantara <nickalcantarasd@gmail.com>
2020-01-13 13:34:07 -0800

    update shipping method

--- src/js/components/checkout.js
+++ src/js/components/checkout.js
@@ -5,6 +5,15 @@ const $parent = $('.k-checkout form');
 const $shippingDetailsTrigger = $('#ship-to-different-address');
 const $drawer = $parent.find('.k-checkout--shipping');
 const $heightTarget = $drawer.find('.k-checkout--shipping__heighttarget');
+const $shippingInputs = $('#shipping_method input');
+
+$shippingInputs.click(function() {
+  $('#order_review').addClass('reloading');
+
+  setTimeout(() => {
+    window.location.reload();
+  }, 2000);
+});
 
 function toggleDrawer(e) {
   if ($drawer.hasClass('open')) {
