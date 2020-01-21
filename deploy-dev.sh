c808f73edb971c9fc8ae4614973dbbf6df2ed108
dougalcantara <dougalcantara619@gmail.com>
2020-01-21 13:27:03 -0800

    rm staging deploy script && update prod script to overwrite instead of delete + reupload

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,2 +1,2 @@
-# before running this, `npm run dev` and wait for CSS/JS to get fully built
+# before running this, `npm run build` and wait for CSS/JS to get fully built
 rsync -r --update --progress --exclude=".*" --exclude-from="./.exclude" ./ koicbddev@koicbddev.ssh.wpengine.net:/home/wpe-user/sites/koicbddev/wp-content/themes/KOI-2019
